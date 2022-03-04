4b346ff1bc99b6a3672c3cc6145f624f9c31820b
roshan0926 <80285716+roshan0926@users.noreply.github.com>
2022-03-04 15:31:58 +0000

    add style

--- src/Products.jsx
+++ src/Products.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './products.css'
 import axios from 'axios';
 const Products = () => {
-  const apiUrl = 'https://global.atdtravel.com/api/products?geo=en'
+  const apiUrl = 'https://atd.atdtravel.com/api/products?geo=en'
   const [products, setProducts] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
   useEffect(() => {
@@ -16,13 +16,17 @@ const Products = () => {
 
     return (
       <div>
-        <h1>Product Search</h1>
-        <span htmlFor='search'>Title:</span>
-        <input onChange={(e) => { setSearchTerm(e.target.value) }} type='text' id='search' placeholder='Search...'></input>
-        <button>Search</button>
-        <table>
-          <thead>
-            <tr>
+        <header className="header">
+          <h1>Product Search</h1>
+          <div className="form">
+            <span htmlFor='search'>Title:</span>
+            <input onChange={(e) => { setSearchTerm(e.target.value) }} type='text' id='search' placeholder='Search...'></input>
+            <button>Search</button>
+          </div>
+        </header>
+        <table className="table">
+          <thead >
+            <tr className="titles">
               <th>Image</th>
               <th>Title</th>
               <th>Destination</th>
@@ -37,36 +41,29 @@ const Products = () => {
                   return val
                 }
               }).map((product) => {
-                              return( <tr>
-              <td key={product.img_sml}><img src={product.img_sml}></img></td>
-              <td key={product.title}>{product.title}</td>
-              <td key={product.dest}>{product.dest}</td>
-            </tr>)
+                return (<tr className="table-items">
+                  <td key={product.img_sml}><img src={product.img_sml}></img></td>
+                  <td key={product.title}>{product.title}</td>
+                  <td key={product.dest}>{product.dest}</td>
+                </tr>)
               })
-              //   products.map((val, product) => {
-              //     products.filter((val) => {
-              //       console.log(val)
-              //     if (searchTerm == "") {
-              //       return val
-              //     } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-              //       return val
-              //     }
-              //   })
-              //   return( <tr>
-              //   <td key={product.img_sml}><img src={product.img_sml}></img></td>
-              //   <td key={product.title}>{product.title}</td>
-              //   <td key={product.dest}>{product.dest}</td>
-              // </tr>)
-              //   })
             }
-
           </tbody>
         </table>
       </div>
     )
 
   } else {
-    return <div>no product</div>
+    return (
+      <header className="header">
+        <h1>Product Search</h1>
+        <div className="form">
+          <span htmlFor='search'>Title:</span>
+          <input onChange={(e) => { setSearchTerm(e.target.value) }} type='text' id='search' placeholder='Search...'></input>
+          <button>Search</button>
+        </div>
+      </header>
+    )
   }
 
 }
